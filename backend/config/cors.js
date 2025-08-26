58c37a8b75eaa01ac107e393987644c7373e07ec
jackmacha914-hash <jackmacha914@gmail.com>
2025-08-26 10:45:37 +0300

    Initial commit - School Management System

---
+++ backend/config/cors.js
@@ -0,0 +1,15 @@
+// List of allowed origins for CORS
+const allowedOrigins = [
+    'http://localhost:5000',
+    'http://127.0.0.1:5000',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080'
+];
+
+module.exports = {
+    allowedOrigins
+};
